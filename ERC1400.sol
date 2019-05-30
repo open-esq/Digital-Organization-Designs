9823b21b68431d87a6c606eab6716238af55e09b
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2019-05-30 11:50:20 -0400

    Update ERC1400.sol
    Add missing IERC1410.sol

--- ERC1400.sol
+++ ERC1400.sol
@@ -541,6 +541,53 @@ interface IERC777TokensRecipient {
 }
 
 /**
+ * @title IERC1410 partially fungible token standard
+ * @dev ERC1410 interface
+ */
+interface IERC1410 {
+
+    // Token Information
+    function balanceOfByPartition(bytes32 partition, address tokenHolder) external view returns (uint256); // 1/10
+    function partitionsOf(address tokenHolder) external view returns (bytes32[]); // 2/10
+
+    // Token Transfers
+    function transferByPartition(bytes32 partition, address to, uint256 value, bytes data) external returns (bytes32); // 3/10
+    function operatorTransferByPartition(bytes32 partition, address from, address to, uint256 value, bytes data, bytes operatorData) external returns (bytes32); // 4/10
+
+    // Default Partition Management
+    function getDefaultPartitions(address tokenHolder) external view returns (bytes32[]); // 5/10
+    function setDefaultPartitions(bytes32[] partitions) external; // 6/10
+
+    // Operators
+    function controllersByPartition(bytes32 partition) external view returns (address[]); // 7/10
+    function authorizeOperatorByPartition(bytes32 partition, address operator) external; // 8/10
+    function revokeOperatorByPartition(bytes32 partition, address operator) external; // 9/10
+    function isOperatorForPartition(bytes32 partition, address operator, address tokenHolder) external view returns (bool); // 10/10
+
+    // Transfer Events
+    event TransferByPartition(
+        bytes32 indexed fromPartition,
+        address operator,
+        address indexed from,
+        address indexed to,
+        uint256 value,
+        bytes data,
+        bytes operatorData
+    );
+
+    event ChangedPartition(
+        bytes32 indexed fromPartition,
+        bytes32 indexed toPartition,
+        uint256 value
+    );
+
+    // Operator Events
+    event AuthorizedOperatorByPartition(bytes32 indexed partition, address indexed operator, address indexed tokenHolder);
+    event RevokedOperatorByPartition(bytes32 indexed partition, address indexed operator, address indexed tokenHolder);
+
+}
+
+/**
  * @title ERC777
  * @dev ERC777 logic
  */
@@ -974,6 +1021,7 @@ contract ERC777 is IERC777, Ownable, ERC820Client, CertificateController, Reentr
   }
 
 }
+
 
 /**
  * @title ERC1410
