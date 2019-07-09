4551d892bd9b79496523f251e99594e420b9e15e
Austin <austinninja362@gmail.com>
2019-07-09 12:11:04 -0400

    Updated Factory Functionality

--- Basic Solidity Factory Pattern.sol
+++ Basic Solidity Factory Pattern.sol
@@ -4,6 +4,7 @@ contract Bakery {
 
   // index of created contracts
 
+  mapping (address => bool) public validContracts; 
   address[] public contracts;
 
   // useful to know the row count in contracts index
@@ -16,28 +17,62 @@ contract Bakery {
     return contracts.length;
   }
 
+  //get all contracts
+
+  function getDeployedContracts() public view returns (address[])
+  {
+    return contracts;
+  }
+
   // deploy a new contract
 
-  function newCookie()
+  function newCookie(string flavor)
     public
-    returns(address newContract)
+    returns(address)
   {
-    Cookie c = new Cookie();
+    Cookie c = new Cookie(msg.sender, flavor);
+    validContracts[c] = true;
     contracts.push(c);
     return c;
   }
+
+  // access child functions
+
+  function getFlavor(address cookie)
+    public
+    view
+    returns(string)
+  {
+    //ensure valid address
+    require(validContracts[cookie],"Contract Not Found!");
+
+    return (Cookie(cookie).getFlavor());
+  }
+
+
 }
 
-
 contract Cookie {
+  
+  address public owner;
+  address public factory;
+  string public flavor;
+
+  // standard constructor
+
+  constructor (address _owner, string _flavor) public {
+    owner = _owner;
+    factory = msg.sender;
+    flavor = _flavor;
+  }
 
   // suppose the deployed contract has a purpose
 
   function getFlavor()
     public
-    pure
-    returns (string memory flavor)
+    view
+    returns (string)
   {
-    return "mmm ... chocolate chip";
+    return flavor;
   }    
 }
