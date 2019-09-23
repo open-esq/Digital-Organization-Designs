315015e006ecd061e816e03cb315da0c5d67f3f8
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2019-09-22 20:52:09 -0400

    Update DigitalSecretary.sol
    abridge IERC20 // add remaining digital secretary functions

--- DigitalSecretary.sol
+++ DigitalSecretary.sol
@@ -1,50 +1,6 @@
 pragma solidity 0.5.3;
 
-interface IERC20 {
-	/**
- 	* @dev Returns the amount of tokens in existence.
- 	*/
-	function totalSupply() external view returns (uint256);
-
-	/**
- 	* @dev Returns the amount of tokens owned by `account`.
- 	*/
-	function balanceOf(address account) external view returns (uint256);
-
-	/**
- 	* @dev Moves `amount` tokens from the caller's account to `recipient`.
- 	*
- 	* Returns a boolean value indicating whether the operation succeeded.
- 	*
- 	* Emits a {Transfer} event.
- 	*/
-	function transfer(address recipient, uint256 amount) external returns (bool);
-
-	/**
- 	* @dev Returns the remaining number of tokens that `spender` will be
- 	* allowed to spend on behalf of `owner` through {transferFrom}. This is
- 	* zero by default.
- 	*
- 	* This value changes when {approve} or {transferFrom} are called.
- 	*/
-	function allowance(address owner, address spender) external view returns (uint256);
-
-	/**
- 	* @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
- 	*
- 	* Returns a boolean value indicating whether the operation succeeded.
- 	*
- 	* IMPORTANT: Beware that changing an allowance with this method brings the risk
- 	* that someone may use both the old and the new allowance by unfortunate
- 	* transaction ordering. One possible solution to mitigate this race
- 	* condition is to first reduce the spender's allowance to 0 and set the
- 	* desired value afterwards:
- 	* https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
- 	*
- 	* Emits an {Approval} event.
- 	*/
-	function approve(address spender, uint256 amount) external returns (bool);
-
+interface abridgedIERC20 {
 	/**
  	* @dev Moves `amount` tokens from `sender` to `recipient` using the
  	* allowance mechanism. `amount` is then deducted from the caller's
@@ -55,20 +11,6 @@ interface IERC20 {
  	* Emits a {Transfer} event.
  	*/
 	function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-
-	/**
- 	* @dev Emitted when `value` tokens are moved from one account (`from`) to
- 	* another (`to`).
- 	*
- 	* Note that `value` may be zero.
- 	*/
-	event Transfer(address indexed from, address indexed to, uint256 value);
-
-	/**
- 	* @dev Emitted when the allowance of a `spender` for an `owner` is set by
- 	* a call to {approve}. `value` is the new allowance.
- 	*/
-	event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
 contract DigitalSecretary {
@@ -135,7 +77,7 @@ contract DigitalSecretary {
         string memory registeredAgentdetails,
         string memory filingDetails) public {
             
-        IERC20 feeToken = IERC20(feeTokenAddress);
+        abridgedIERC20 feeToken = abridgedIERC20(feeTokenAddress);
         
         assert(feeToken.transferFrom(msg.sender, address(secretary), feeAmount));
         
@@ -163,22 +105,40 @@ contract DigitalSecretary {
             
             emit entityRegistered(fileNumber, filingDate, entityName, entityKind, domestic);
         }
-
+   
+    // digital secretary can update entity name   
+    function updateEntityName(uint256 fileNumber, string memory newName) public onlySecretary {
+        Entity storage entity = entities[fileNumber];
+        entity.entityName = newName; 
+    }
+    
+    // digital secretary can update entity name   
+    function updateRegisteredAgent(uint256 fileNumber, string memory registeredAgentdetails) public onlySecretary {
+        Entity storage entity = entities[fileNumber];
+        entity.registeredAgentdetails = registeredAgentdetails; 
+    }
+    
     // digital secretary can convert entity kind   
     function convertEntityKind(uint256 fileNumber, uint8 newKind) public onlySecretary {
         Entity storage entity = entities[fileNumber];
         entity.entityKind = newKind; 
     }
-
-    // digital secretary can reduce entity standing for non-compliance  
-    function reduceStanding(uint256 fileNumber) public onlySecretary {
+    
+    // digital secretary can convert entity kind   
+    function convertEntityType(uint256 fileNumber, uint8 newType) public onlySecretary {
         Entity storage entity = entities[fileNumber];
-        entity.goodStanding = false; 
+        entity.entityType = newType; 
     }
     
-    // digital secretary can repair entity standing upon compliance  
-    function repairStanding(uint256 fileNumber) public onlySecretary {
+    // digital secretary can convert entity domicile   
+    function convertEntityDomicile(uint256 fileNumber, bool domestic) public onlySecretary {
         Entity storage entity = entities[fileNumber];
-        entity.goodStanding = true; 
+        entity.domestic = domestic; 
+    }
+
+    // digital secretary can convert entity kind   
+    function convertEntityStanding(uint256 fileNumber, bool goodStanding) public onlySecretary {
+        Entity storage entity = entities[fileNumber];
+        entity.goodStanding = goodStanding; 
     }
 }
